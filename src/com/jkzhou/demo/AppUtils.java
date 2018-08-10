68798519646f1d224497dbce345f0ecc44bd8139
J.K. Zhou <ktalex1993@gmail.com>
2018-08-09 22:26:40 -0400

    Remove class header comment

--- src/com/jkzhou/demo/AppUtils.java
+++ src/com/jkzhou/demo/AppUtils.java
@@ -1,13 +1,3 @@
-// **********************************************************************
-// Copyright (c) 2018 Telefonaktiebolaget LM Ericsson, Sweden.
-// All rights reserved.
-// The Copyright to the computer program(s) herein is the property of
-// Telefonaktiebolaget LM Ericsson, Sweden.
-// The program(s) may be used and/or copied with the written permission
-// from Telefonaktiebolaget LM Ericsson or in accordance with the terms
-// and conditions stipulated in the agreement/contract under which the
-// program(s) have been supplied.
-// **********************************************************************
 package com.jkzhou.demo;
 
 public final class AppUtils
@@ -24,7 +14,9 @@ public final class AppUtils
 
     public static Console getConsole(Class<?> callerType)
     {
-        return (format, args) -> System.out.printf("[%s] : %s%n", callerType.getSimpleName(), String.format(format, args));
+        return (format, args) -> System.out.printf("[%s] : %s%n",
+                callerType.getSimpleName(),
+                String.format(format, args));
     }
 
     private AppUtils()
